61ff0c01da5c1a0aa900c31ad1a67800f3048b2f
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2021-06-11 15:14:54 +0200

    comment löschen

--- script.js
+++ script.js
@@ -213,44 +213,36 @@ const mathefragen =
             answers: [ {text:"1",value:0},{text:"3",value:0},{text: "4",value: 1},{text: "0",value: 0}]
 
         },
-//2
+
         {
             text:  "10(x-9)=-10",
             answers: [ {text:"39",value:0},{text:"21",value:0},{text: "8",value: 1},{text: "6",value: 0}]
 
         },
 
-//3
-        {
-            text:  "\\textit{Welche von der vier  Trigonometrie-Formeln ist falsch geschrieben?} \\\\ 1.) \\tg{x}\\ctg{x}=1 \\\\2.) \\sin^2x + \\cos^2x = 1  \\\\3.) \\tg^2{x}={\\sin{x} \\over \\cos{x} }\\\\4.) \\sin{3x}=3\\sin{x}-4\\sin^3{x} ",
-            answers: [ {text:"1",value:0},{text:"2",value:0},{text: "3",value: 1},{text: "4",value: 0}]
 
-        },
-  //4
+ 
         {
             text:  "\\textit{ Berechnen Sie den Grenzwert}\\\\ \\\\ \\lim\\limits_{x\\to\\infty } ({4x+7 \\over 2x+3})^{4-x}",
             answers: [ {text:"+ ∞",value:1},{text:"8e",value:0},{text: "0",value: 0},{text: "1",value: 0}]
 
         },
-//5
+
         {
             text:  "\A=\\begin{pmatrix} 3 & -1 \\\\ 4 & 2 \\end{pmatrix}, \B=\\begin{pmatrix} 3 & 4 \\\\ 1 & 3 \\end{pmatrix} \\textit{,dann } \\\\ \\\\ \\textit{Determinante } (\A^{-1} * \B)^{-2}=",
             answers: [ {text:"-0.5",value:0},{text:"4",value:0},{text: "-4",value: 1},{text: "0.5",value: 0}]
 
         },
-//6
+
     {
         text:  `2 * 10^{-3} =`,
         answers: [ {text:"0,002",value:1},{text:"0,2",value:0},{text: "200",value: 0},{text: "2000",value: 0}]
 
     },
-    {
-        text:  "\\textit{Wie hoch ist dein Sparguthaben, wenn du bei einem} \\\\ \\textit{Zinssatz von 1,5  \\% im Jahr Zinsen in Höhe von 7,50 € erhältst?}",
-        answers: [ {text:"500 €",value:1},{text:"550 €",value:0},{text: "600 €",value: 0},{text: "825 €",value: 0}]
-    },
+  
 
     {
-        text:  "\\textit{Der Durchmesser eines Reifens beträgt 60 cm. Wie groß ist sein Umfang?}",
+        text:  "\\textit{Der Durchmesser eines Reifens beträgt 60 cm}. \\\\ \\textit{Wie groß ist sein Umfang?}",
         answers: [ {text:"19,1 cm",value:0},{text:"94,2 cm",value:0},{text: "102,25 cm",value: 0},{text: "188,4 cm",value: 1}]
     }
 ];
@@ -258,7 +250,7 @@ const mathefragen =
 
 const internettechnologien =
     [
-//1
+
     {
         text:  "Welches der folgenden Protokolle ist ein Internetprotokoll?",
         answers: [ {text:"HTTP",value:1},{text:"HTML",value:0},{text: "CSS",value: 0},{text: "UTP",value: 0}]
